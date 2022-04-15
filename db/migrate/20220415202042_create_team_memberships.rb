e4b6c3571557873248b79f0571e2495586cf770e
Rajat Gupta <gupta.1027@osu.edu>
2022-04-15 17:18:48 -0400

    added belongs_to to create_team_project and create_tam_membership in migrate

--- db/migrate/20220415202042_create_team_memberships.rb
+++ db/migrate/20220415202042_create_team_memberships.rb
@@ -1,6 +1,8 @@
 class CreateTeamMemberships < ActiveRecord::Migration[6.1]
   def change
     create_table :team_memberships do |t|
+    t.belongs_to :team
+    t.belongs_to :student
     t.timestamps
     end
   end
--- db/migrate/20220415204518_create_team_projects.rb
+++ db/migrate/20220415204518_create_team_projects.rb
@@ -4,7 +4,8 @@ class CreateTeamProjects < ActiveRecord::Migration[6.1]
     create_table :team_projects do |t|
       t.integer :grade
       t.boolean :evals_complete
-
+			t.belongs_to :team
+			t.belogs_to :project
       t.timestamps
     end
   end
