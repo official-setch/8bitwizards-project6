c00e261b37e3b2a171944db0d59d51602e9ec9e0
eijnehs <eijnehs@outlook.com>
2022-04-16 19:32:32 -0400

    add the team page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
-  
+
   def index
     @users = User.all
   end
@@ -17,6 +17,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    
+    if params[:user][:admin] == '1'
+       @user.admin = true
+    end
+    
     if @user.save
       log_in @user
       flash[:success] = "Account created successfully!"
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,3 +10,4 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery")
 import "bootstrap"
+
