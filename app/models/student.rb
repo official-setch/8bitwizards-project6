e6ffcf8a2560a4d2bbedc4264275f22aa833acc9
Rajat Gupta <gupta.1027@osu.edu>
2022-04-16 16:44:31 -0400

    added student, admin, eval models

---
+++ app/models/student.rb
@@ -0,0 +1,6 @@
+class Student < ApplicationRecord
+  has_many :team_memberships
+  has_many :teams, through: :team_memberships
+    limit:2
+    
+end
---
+++ db/migrate/20220412181627_create_admins.rb
@@ -0,0 +1,12 @@
+class CreateAdmins < ActiveRecord::Migration[6.1]
+  def change
+    create_table :admins do |t|
+    
+      t.string :fname
+      t.string :lname
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
