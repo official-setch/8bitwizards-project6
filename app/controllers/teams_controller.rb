c4698fd888c81c7f42e2266f260796c42a60c48e
eijnehs <eijnehs@outlook.com>
2022-04-17 13:23:58 -0400

    add the team page and be able to add teams and delete teams

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,79 +1,59 @@
 class TeamsController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
-  before_action :correct_user, only: [:edit, :update]
 
+  
   def index
-    @users = User.all
+    @teams = Team.all
   end
   
   
   def show
-    @user = User.find(params[:id])
+    @team = Team.find(params[:id])
   end
 
   def new
-    @user = User.new
+    @team = Team.new
   end
 
+
   def create
-    @user = User.new(user_params)
-    
-    if params[:user][:admin] == '1'
-       @user.admin = true
-    end
-    
-    if @user.save
-      log_in @user
-      flash[:success] = "Account created successfully!"
-      redirect_to @user
+    @team = Team.new(team_params)
+
+    if @team.save
+      flash[:success] = "Team created successfully!"
+      redirect_to @team
     else
       render 'new'
     end
   end
-
+  
   def edit
-    @user = User.find(params[:id])
+    @team = Team.find(params[:id])
   end
   
-   def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      flash[:success] = "Profile updated"
-      redirect_to @user
+  
+  def update
+    @team = Team.find(params[:id])
+    if @team.update(team_params)
+      flash[:success] = "Team updated"
+      redirect_to @team
     else
       render 'edit'
     end
   end
   
-
-  
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User deleted"
-    redirect_to users_url
+    Team.find(params[:id]).destroy
+    flash[:success] = "Team deleted"
+    redirect_to teams_url
   end
   
   
   private
+    def team_params
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
     end
+
     
-    # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
     
-    # Confirms the correct user.
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
-    end
-    
+
 end
