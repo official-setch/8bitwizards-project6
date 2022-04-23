1c59ab0469fdd3efaa2c98568a49589f50fef2cf
Rajat Gupta <gupta.1027@osu.edu>
2022-04-23 02:58:08 -0400

    users can fill out eval forms, instructor can see the results

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,21 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  #get 'evals/index'
+  #get 'projects/complete'
+  post 'surveys/index' => 'surveys#create'
+  post 'evals/complete' => 'evals#create'
+  post 'evals/index' => 'evals#create'
+  
+  get 'complete/:id' => 'evals#complete'
+  #get 'evals/display' => 'evals#display'
+  post 'complete/:id' => 'evals#create'
+  get 'evals/display/:id' => 'evals#display'
+  
   resources :users
   resources :teams
   resources :team_memberships
   resources :projects
+  resources :evals
+  
 end
