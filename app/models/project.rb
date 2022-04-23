12995965cb1106272282bf272f96c2563ab4edd5
Seth Walczak <92406170+official-setch@users.noreply.github.com>
2022-04-23 14:37:06 -0400

    Added documentation

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ApplicationRecord
+  #Model for a single project. Many to many relation with teams, through tertiary table :team_to_projects
   has_many :team_to_projects
   has_many :teams, through: :team_to_projects
 end
